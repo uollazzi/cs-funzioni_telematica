294a584326c27f78a9381be81f8c95250d9aedd7
Matteo Valenzi <dilloawallace@hotmail.com>
2025-11-06 12:41:24 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,76 +1,15 @@
-﻿int AggiungiUno(int numero)
+﻿// lamba
+// serve per creare funzioni anonime (usa e getta)
+// action (non ritornano un valore)
+// func (ritornano un valore)
+List<int> numeri = [1, 2, 3, 4];
+
+numeri.ForEach(x =>
 {
-    return numero + 1;
-}
+    Console.WriteLine(x);
+});
 
-int Somma(int n1, int n2)
-{
-    return n1 + n2;
-}
+var quadrati = numeri.Select(x => x * x);
 
-var risultato = Somma(3, 5);
-
-risultato = Somma(n2: 6, n1: 9);
-
-// C# passa i parametri come valore
-// cioè di default, passa una copia della variabile
-void quadratoByVal(int valParameter)
-{
-    valParameter *= valParameter;
-}
-
-int numero = 4;
-quadratoByVal(numero);
-Console.WriteLine(numero);
-
-void quadratoByRef(ref int refParameter)
-{
-    refParameter *= refParameter;
-}
-quadratoByRef(ref numero);
-Console.WriteLine(numero);
-
-// output
-string dieci = "10";
-int rr;
-bool ok = int.TryParse(dieci, out rr);
-Console.WriteLine($"Riuscito: {ok}, risultato: {rr}");
-
-// numero parametri variabile
-int SommaMultipla(params int[] numeri)
-{
-    int r = 0;
-    foreach (var n in numeri)
-    {
-        r += n;
-    }
-
-    return r;
-}
-
-Console.WriteLine(SommaMultipla(5, 7, 4, 8));
-Console.WriteLine(SommaMultipla(5, 7, 4, 8, 7, 34, 56, 7));
-
-// parametri di default (facoltativi)
-void Saluta(string nome = "a tutti")
-{
-    Console.WriteLine($"Ciao {nome}");
-}
-Saluta("Pippo");
-Saluta();
-
-void SalutaNVolte(string nome, string saluto = "Ciao", int volte = 3)
-{
-    for (int i = 0; i < volte; i++)
-    {
-        Console.WriteLine($"{saluto} {nome}");
-    }
-}
-
-SalutaNVolte("Bruno");
-SalutaNVolte("Vario", "Salve");
-SalutaNVolte("Anna", "Buongiorno", 8);
-
-// salutare Luisa 5 volte senza sovrascrivere "Ciao"
-SalutaNVolte("Luisa", volte: 5);
+Console.WriteLine(string.Join(",", quadrati));
 
