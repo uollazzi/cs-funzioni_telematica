a64d827adfa54d8cb7bca20d5586072ff453e8d8
Matteo Valenzi <dilloawallace@hotmail.com>
2025-11-13 09:05:44 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,73 +1,12 @@
-﻿using System.Globalization;
+﻿/*
+Scrivere un programma che visualizzi la cartella "documenti"
+in console come da esempio nel file screenshot.png
 
-void LogTitolo(string titolo)
-{
-    Console.WriteLine();
-    Console.ForegroundColor = ConsoleColor.DarkGreen;
-    Console.WriteLine(titolo + ":");
-    Console.ResetColor();
-}
-
-var oggi = DateTime.Now;
-LogTitolo("Data di oggi");
-Console.WriteLine(oggi);
-
-var ora = oggi.TimeOfDay;
-LogTitolo("Orario");
-Console.WriteLine(ora);
-
-// creare una data
-var scopertaAmerica = new DateTime(1492, 10, 12);
-var durata = new TimeSpan(23, 16, 0); // durata, un lasso di tempo (usata anche per definire le ore)
-
-scopertaAmerica = scopertaAmerica.Add(durata);
-LogTitolo("Date e ora scoperta America");
-Console.WriteLine(scopertaAmerica);
-
-// DateTimeOffset = Date + Time + Offset
-var oggiUTC = DateTimeOffset.UtcNow;
-Console.WriteLine(oggiUTC);
-Console.WriteLine(oggiUTC.LocalDateTime);
-
-var data = DateTimeOffset.FromUnixTimeSeconds(1762941726);
-Console.WriteLine(data.LocalDateTime);
-
-Console.WriteLine(((DateTimeOffset)scopertaAmerica).ToUnixTimeSeconds());
-
-// formattazione
-LogTitolo("DateTime formattazione");
-Console.WriteLine(oggi.ToString());
-Console.WriteLine(oggi.ToShortDateString());
-Console.WriteLine(oggi.ToLongDateString());
-Console.WriteLine(oggi.ToShortTimeString());
-Console.WriteLine(oggi.ToString("ddd, dd MMMM yyyy HH:mm"));
-Console.WriteLine(oggi.ToString("ddd, dd MMMM yyyy HH:mm", new CultureInfo("en-EN")));
-
-// parsing
-DateTime d;
-bool isSuccess = DateTime.TryParse("22/10/2025", out d);
-
-LogTitolo("Parsing data");
-Console.WriteLine(isSuccess);
-Console.WriteLine(d);
-
-isSuccess = DateTime.TryParse("10-22-2025", new CultureInfo("en-US"), out d);
-
-LogTitolo("Parsing data");
-Console.WriteLine(isSuccess);
-Console.WriteLine(d);
-
-isSuccess = DateTime.TryParseExact("10-22-2025 12:37", "MM-dd-yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out d);
-
-LogTitolo("Parsing data");
-Console.WriteLine(isSuccess);
-Console.WriteLine(d.ToString());
-
-var ieri = oggi.AddDays(-1);
-LogTitolo("Ieri");
-Console.WriteLine(ieri);
-
-LogTitolo("Da quanto esiste l'America?");
-var tempoPassato = oggi - scopertaAmerica;
-Console.WriteLine((int)(tempoPassato.TotalDays / 365));
-Console.WriteLine(Math.Floor(tempoPassato.TotalDays / 365));
+- Creare un programma dinamico,
+  che cioè si adatti alla struttura delle cartelle nel momento in cui le modifichiamo
+  senza che sia necessario riscrivere il programma
+- Cercate di utilizzare delle funzioni per parti del programma che si ripetono.
+- Le cartelle devono essere visualizzate con un colore diverso dai files.
+- I files modificati da meno di 5 minuti devono assumere un colore diverso
+- I files modificati da meno di 30 minuti (ma da più di 5) devono assumere un colore ancora diverso
+*/
